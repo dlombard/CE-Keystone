efb0c0325fd08820f3686311ab694a99f189fc78
Davenson Lombard <lombard.davenson@gmail.com>
2017-03-22 23:46:34 -0400

    Indentation fix

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -35,24 +35,24 @@ module.exports = {
   plugins: [
     // added this thing!
     new webpack.HotModuleReplacementPlugin(),
-     new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }), new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: true,
-                screw_ie8: true,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true,
-            }
-        }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }), new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      }
+    }),
   ],
 }
 
