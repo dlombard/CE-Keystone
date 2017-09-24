7c56e4ce31f0cf6d99b38c394906cf3f630537be
Davenson Lombard <lombard.davenson@gmail.com>
2017-09-23 22:12:08 -0400

    Removed unnecessary app

--- keystone.js
+++ keystone.js
@@ -4,36 +4,9 @@ require('dotenv').config();
 const logger = require('./logger');
 // Require keystone
 const keystone = require('keystone');
-const Email = require('keystone-email')
-const handlebars = require('express-handlebars');
-const compression = require('compression');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const mongoose = require('mongoose')
-const Promise = require('bluebird')
-const requestIp = require('request-ip')
-const history = require('connect-history-api-fallback');
-const app = express();
+const MongoDBStore = require('connect-mongodb-session')(session);
 
-app.get('/js/bundle.js', (req, res) => {
-	res.setHeader('Content-Type', 'application/javascript');
-	res.sendFile('bundle.js', { root: `${__dirname}/` });
-});
-//app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(compression());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use('/styles/', express.static(path.resolve(__dirname, '..', 'css')));
-app.use(require('morgan')("combined", { "stream": logger.stream }));
 keystone.init({
 	'name': 'Cesperance',
 	'brand': 'Cesperance',
@@ -41,17 +14,26 @@ keystone.init({
 	'static': 'public',
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
-	'view engine': 'pug',
+	'view engine': 'ejs',
 	'auto update': true,
 	'session': true,
-	'session store': 'mongo',
 	'auth': true,
 	'user model': 'User',
 	'wysiwyg additional buttons': 'preview',
 	'wysiwyg additional plugins': 'preview',
+	'mongo options': { useMongoClient: true, },
+	'logger': 'combined',
+	'logger options': { "stream": logger.stream },
 	'host': process.env.HOST || '127.0.0.1',
 	'port': process.env.PORT,
-	'mongo options': { useMongoClient: true, }
+	'mongo': process.env.MONGO_URI,
+	'session store': (session) => {
+		return new MongoDBStore(
+			{
+				uri: process.env.MONGO_URI,
+				collection: 'sessions'
+			});
+	}
 });
 
 //keystone.app = app
@@ -63,12 +45,12 @@ keystone.set('locals', {
 	editable: keystone.content.editable,
 });
 
-//keystone.set('email nodemailer', transport)
 keystone.set('routes', require('./lib/server/routes/react'));
 
 keystone.set('nav', {
 	users: 'users',
-	songs: 'songs'
+	songs: 'songs',
+	books: 'books'
 });
 
 
